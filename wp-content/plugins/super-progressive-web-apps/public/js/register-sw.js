e15879a4aa0ecfa51f2e0a03037fd89d09b5aee9
Osama Yousuf <oy02945@st.habib.edu.pk>
2019-01-14 16:51:47 +0500

    Fixed PWA Bugs iOS

--- wp-content/plugins/super-progressive-web-apps/public/js/register-sw.js
+++ wp-content/plugins/super-progressive-web-apps/public/js/register-sw.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
 	navigator.serviceWorker.register(superpwa_sw.url)
-	.then(function(registration) {console.log('LOL URL: ' + superpwa_sw.url); console.log('SuperPWA service worker ready'); registration.update(); })
+	.then(function(registration) {console.log(console.log('SuperPWA service worker ready'); registration.update(); })
 	.catch(function(error) { console.log('Registration failed with ' + error); });
   });
 }
